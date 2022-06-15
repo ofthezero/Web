c32c0d854bd50c379959ff4d25d9f2ecd0172185
AAA RRR <bestpositive@mail.ru>
2022-06-15 14:09:56 +0300

    web

---
+++ app/src/main/java/mpt/ru/mar/web/MainActivity.java
@@ -0,0 +1,49 @@
+package mpt.ru.mar.web;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+    ProgressBar progress;
+    String url = "https://yandex.ru";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        web = findViewById(R.id.web);
+        progress = findViewById(R.id.progress);
+        web.setWebViewClient(new WebClient());
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl(url);
+    }
+
+    public class WebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            progress.setVisibility(View.VISIBLE);
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progress.setVisibility(View.GONE);
+        }
+    }
+}
